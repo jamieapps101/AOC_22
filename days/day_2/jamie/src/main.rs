5adb1934e09478d935345b932bcf79ed38c4c923
jamie apps <jamieapps101@gmail.com>
2022-12-02 13:58:37 +0000

    Day 2: Challenge 2

--- days/day_2/jamie/src/main.rs
+++ days/day_2/jamie/src/main.rs
@@ -4,12 +4,13 @@ fn main() {
     let input_lines = io::stdin().lines();
     let score: u32 = input_lines
         .filter_map(|l| l.ok())
-        .map(|l| line_to_score(l.trim()))
+        .map(|l| line_to_score_reverse(l.trim()))
         .sum();
     println!("score: {score}");
 }
 
 #[cfg_attr(test, derive(Debug))]
+#[derive(Copy, Clone)]
 enum Item {
     Rock,
     Paper,
@@ -28,11 +29,20 @@ impl Item {
         let self_index: usize = self.into();
         let other_index: usize = other.into();
         let lu = [
-            [GameResult::Draw, GameResult::Loose, GameResult::Win], // Rock
-            [GameResult::Win, GameResult::Draw, GameResult::Loose], // Paper
-            [GameResult::Loose, GameResult::Win, GameResult::Draw], // Scissors
+            [GameResult::Draw, GameResult::Lose, GameResult::Win], // Rock
+            [GameResult::Win, GameResult::Draw, GameResult::Lose], // Paper
+            [GameResult::Lose, GameResult::Win, GameResult::Draw], // Scissors
         ];
         lu[self_index][other_index]
+    }
+
+    fn required(other: Self, result: GameResult) -> Self {
+        let other_index: usize = other.into();
+        match result {
+            GameResult::Win => [Item::Paper, Item::Scissors, Item::Rock][other_index],
+            GameResult::Draw => [Item::Rock, Item::Paper, Item::Scissors][other_index],
+            GameResult::Lose => [Item::Scissors, Item::Rock, Item::Paper][other_index],
+        }
     }
 }
 
@@ -65,7 +75,7 @@ impl From<char> for Item {
 #[cfg_attr(test, derive(PartialEq, Debug))]
 enum GameResult {
     Win,
-    Loose,
+    Lose,
     Draw,
 }
 
@@ -74,11 +84,23 @@ impl GameResult {
         match self {
             Self::Win => 6,
             Self::Draw => 3,
-            Self::Loose => 0,
+            Self::Lose => 0,
         }
     }
 }
 
+impl From<char> for GameResult {
+    fn from(c: char) -> Self {
+        match c {
+            'X' => Self::Lose,
+            'Y' => Self::Draw,
+            'Z' => Self::Win,
+            _ => panic!("Could not convert {c} to GameResult"),
+        }
+    }
+}
+
+#[allow(dead_code)]
 fn line_to_score(line: &str) -> u32 {
     if line.len() != 3 {
         panic!("Could not understand {line}");
@@ -94,18 +116,35 @@ fn line_to_score(line: &str) -> u32 {
     self_score + game_score
 }
 
+fn line_to_score_reverse(line: &str) -> u32 {
+    if line.len() != 3 {
+        panic!("Could not understand {line}");
+    }
+    let mut chars = line.chars();
+    let other_item: Item = chars.next().unwrap().into();
+    let _ = chars.next();
+    let game_result: GameResult = chars.next().unwrap().into();
+
+    let self_item = Item::required(other_item, game_result);
+
+    let self_score = self_item.self_score();
+
+    let game_score = game_result.to_score();
+    self_score + game_score
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
     #[test]
     fn test_item_outcome() {
         assert_eq!(Item::Rock.outcome(Item::Rock), GameResult::Draw);
-        assert_eq!(Item::Rock.outcome(Item::Paper), GameResult::Loose);
+        assert_eq!(Item::Rock.outcome(Item::Paper), GameResult::Lose);
         assert_eq!(Item::Rock.outcome(Item::Scissors), GameResult::Win);
         assert_eq!(Item::Paper.outcome(Item::Rock), GameResult::Win);
         assert_eq!(Item::Paper.outcome(Item::Paper), GameResult::Draw);
-        assert_eq!(Item::Paper.outcome(Item::Scissors), GameResult::Loose);
-        assert_eq!(Item::Scissors.outcome(Item::Rock), GameResult::Loose);
+        assert_eq!(Item::Paper.outcome(Item::Scissors), GameResult::Lose);
+        assert_eq!(Item::Scissors.outcome(Item::Rock), GameResult::Lose);
         assert_eq!(Item::Scissors.outcome(Item::Paper), GameResult::Win);
         assert_eq!(Item::Scissors.outcome(Item::Scissors), GameResult::Draw);
     }
