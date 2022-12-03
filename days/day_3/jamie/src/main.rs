251debf25513f1c46ad9e184dad80db78a1a535c
jamie apps <jamieapps101@gmail.com>
2022-12-03 15:19:09 +0000

    Day 3: Challenge 2

--- days/day_3/jamie/src/main.rs
+++ days/day_3/jamie/src/main.rs
@@ -1,15 +1,35 @@
+use std::env;
 use std::io;
 
 fn main() {
+    let mode_str = env::args().into_iter().nth(1).unwrap();
+    let mode = mode_str
+        .parse::<u32>()
+        .unwrap_or_else(|_| panic!("mode = {mode_str}"));
+
     let input_lines = io::stdin().lines().filter_map(|l| l.ok());
-    let score = find_score(input_lines);
+    let score: u32 = match mode {
+        1 => find_score(input_lines),
+        2 => input_lines
+            .chonk::<3>()
+            .filter_map(|group| {
+                let c = find_common_items_between(&group[0], &group[1]);
+                let mut c =
+                    find_common_items_between(&group[2], c.iter().collect::<String>().as_ref());
+                c.dedup();
+                c.pop()
+            })
+            .map(item_to_score)
+            .sum(),
+        _ => panic!("I only work with mode 1 or 2"),
+    };
     println!("Score: {score}");
 }
 
 fn find_score<I: AsRef<str>, S: Iterator<Item = I>>(source: S) -> u32 {
     source
         .filter_map(|l| find_common_item(l.as_ref()))
-        .map(|c| item_to_score(c))
+        .map(item_to_score)
         .sum()
 }
 
@@ -26,14 +46,47 @@ fn item_to_score(item: char) -> u32 {
 fn find_common_item(contents: &str) -> Option<char> {
     let length = contents.len();
     let (compartment_a, compartment_b) = contents.split_at(length / 2);
-    let compartment_a_chars: Vec<char> = compartment_a.chars().collect();
-    let compartment_b_chars: Vec<char> = compartment_b.chars().collect();
-    for cac in compartment_a_chars {
-        if compartment_b_chars.contains(&cac) {
-            return Some(cac);
-        }
+    find_common_item_between(compartment_a, compartment_b)
+}
+
+fn find_common_item_between(a: &str, b: &str) -> Option<char> {
+    let b_chars: Vec<char> = b.chars().collect();
+    a.chars().into_iter().find(|&ac| b_chars.contains(&ac))
+}
+
+fn find_common_items_between(a: &str, b: &str) -> Vec<char> {
+    let a_chars: Vec<char> = a.chars().collect();
+    let b_chars: Vec<char> = b.chars().collect();
+    a_chars
+        .iter()
+        .filter(|ac| b_chars.contains(ac))
+        .copied()
+        .collect()
+}
+
+trait Chonk<I, S: Iterator<Item = I>> {
+    fn chonk<const N: usize>(self) -> Chonker<N, I, S>;
+}
+
+struct Chonker<const N: usize, I, S: Iterator<Item = I>> {
+    source: S,
+}
+
+impl<I, S: Iterator<Item = I>> Chonk<I, S> for S {
+    fn chonk<const N: usize>(self) -> Chonker<N, I, S> {
+        Chonker { source: self }
     }
-    None
+}
+
+impl<const N: usize, I, S: Iterator<Item = I>> Iterator for Chonker<N, I, S> {
+    type Item = Vec<I>;
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut t = Vec::with_capacity(N);
+        for _ in 0..N {
+            t.push(self.source.next()?);
+        }
+        Some(t)
+    }
 }
 
 #[cfg(test)]
