4cc151c6b787c3a056fd3f94f5234e966776e3c6
jamie apps <jamieapps101@gmail.com>
2022-12-04 15:02:47 +0000

    Day 4: Challenge 1

---
+++ days/day_4/jamie/src/main.rs
@@ -0,0 +1,70 @@
+use std::convert::TryFrom;
+use std::io;
+fn main() {
+    let input_lines = io::stdin().lines().filter_map(|l| l.ok());
+    let total_overlapped_groups = input_lines
+        .map(|l| line_to_range_group(l).unwrap())
+        .filter(Range::either_contains)
+        .count();
+    println!("total_overlapped_groups: {total_overlapped_groups}");
+}
+
+#[cfg_attr(test, derive(Debug, PartialEq))]
+struct Range {
+    start: u32,
+    end: u32,
+}
+
+impl Range {
+    fn contains(&self, other: &Self) -> bool {
+        self.start <= other.start && other.end <= self.end
+    }
+
+    fn either_contains(group: &(Range, Range)) -> bool {
+        group.0.contains(&group.1) || group.1.contains(&group.0)
+    }
+}
+
+fn line_to_range_group<S: AsRef<str>>(line: S) -> Option<(Range, Range)> {
+    let mut line_split = line.as_ref().split(',');
+    let range_1 = Range::try_from(line_split.next()?).ok()?;
+    let range_2 = Range::try_from(line_split.next()?).ok()?;
+    Some((range_1, range_2))
+}
+
+impl TryFrom<&str> for Range {
+    type Error = &'static str;
+    fn try_from(s: &str) -> Result<Self, Self::Error> {
+        let mut range = s.split('-');
+        let start = if let Some(v_str) = range.next() {
+            if let Ok(v) = v_str.parse::<u32>() {
+                v
+            } else {
+                return Err("Could not parse a start value from {s}");
+            }
+        } else {
+            return Err("Could not get a start value from {s}");
+        };
+        let end = if let Some(v_str) = range.next() {
+            if let Ok(v) = v_str.parse::<u32>() {
+                v
+            } else {
+                return Err("Could not parse an end value from {s}");
+            }
+        } else {
+            return Err("Could not get an end value from {s}");
+        };
+        Ok(Range { start, end })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_line_to_range_group() {
+        let result = line_to_range_group("17-99,18-24");
+        let ref_result = Some((Range { start: 17, end: 99 }, Range { start: 18, end: 24 }));
+        assert_eq!(result, ref_result);
+    }
+}
