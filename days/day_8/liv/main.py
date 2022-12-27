36ec72ca10852a5a221236f66df592540d7c04e9
justletmeliv <oabwebster@gmail.com>
2022-12-27 17:29:20 +0000

    Day 8: Challenge 2

--- days/day_8/liv/main.py
+++ days/day_8/liv/main.py
@@ -20,7 +20,7 @@ def read_file(file_path: str) -> List[int]:
         # print(all_trees)
     # print(all_trees)
     tree_grid = np.array(all_trees, np.int32)
-    # print(tree_grid)
+    print(tree_grid)
     return tree_grid
 
 
@@ -29,7 +29,7 @@ def visibility_check(tree_grid: List[List[int]]):
     # print(f"Max X = {max_x_dimension}")
     max_y_dimension = np.size(tree_grid, 1)
     # print(f"Max Y = {max_y_dimension}")
-    visible_trees = 0
+    highest_scenic_score = 0
     for x_index in range(tree_grid.shape[0]):
         for y_index in range(tree_grid.shape[1]):
             tree = tree_grid[x_index, y_index]
@@ -55,20 +55,85 @@ def visibility_check(tree_grid: List[List[int]]):
             # print(f"Blocking Right: {blocking_tree_right_count}")
             # print(f"Blocking Up: {blocking_tree_up_count}")
             # print(f"Blocking Down: {blocking_tree_down_count}")
+            left_scenic_score = 0
             if blocking_tree_left_count == 0:
-                # print("Visible from LEFT")
-                visible_trees += 1
-            elif blocking_tree_right_count == 0:
-                # print("Visible from RIGHT")
-                visible_trees += 1
-            elif blocking_tree_up_count == 0:
-                # print("Visible from UP")
-                visible_trees += 1
-            elif blocking_tree_down_count == 0:
-                # print("Visible from DOWN")
-                visible_trees += 1
-    print(visible_trees)
-    return visible_trees
+                # print(f"Left View = {left_view}")
+                # print(f"Size of Left View: {len(left_view)}")
+                left_scenic_score += len(left_view)
+                # print(f"No Blocking Trees - Left Scenic Score:
+                # {left_scenic_score}")
+            elif blocking_tree_left_count != 0:
+                reversed_left_view = np.array(list(reversed(left_view)))
+                # print(f"Reversed Left View = {reversed_left_view}")
+                for pine in reversed_left_view:
+                    # print(f"Pine = {pine}")
+                    if pine < tree:
+                        left_scenic_score += 1
+                    else:
+                        left_scenic_score += 1
+                        # print(f"Left Scenic Score (end for loop):
+                        # {left_scenic_score}")
+                        break
+            right_scenic_score = 0
+            if blocking_tree_right_count == 0:
+                right_scenic_score += len(right_view)
+                # print(f"No Blocking Trees - Right Scenic Score:
+                # {right_scenic_score}")
+            elif blocking_tree_right_count != 0:
+                for pine in right_view:
+                    if pine < tree:
+                        right_scenic_score += 1
+                    else:
+                        right_scenic_score += 1
+                        # print(f"Right Scenic Score (end for loop):
+                        # {right_scenic_score}")
+                        break
+            up_scenic_score = 0
+            if blocking_tree_up_count == 0:
+                up_scenic_score += len(up_view)
+                # print(f"No Blocking Trees - Up Scenic Score:
+                # {up_scenic_score}")
+            elif blocking_tree_up_count != 0:
+                reversed_up_view = np.array(list(reversed(up_view)))
+                for pine in reversed_up_view:
+                    if pine < tree:
+                        up_scenic_score += 1
+                    else:
+                        up_scenic_score += 1
+                        # print(f"Up Scenic Score (end for loop):
+                        # {up_scenic_score}")
+                        break
+            down_scenic_score = 0
+            if blocking_tree_down_count == 0:
+                down_scenic_score += len(down_view)
+                # print(f"No Blocking Trees - Down Scenic Score:
+                # {down_scenic_score}")
+            elif blocking_tree_down_count != 0:
+                for pine in down_view:
+                    if pine < tree:
+                        down_scenic_score += 1
+                    else:
+                        down_scenic_score += 1
+                        # print(f"Down Scenic Score (end for loop):
+                        # {down_scenic_score}")
+                        break
+            # print(f"Left Scenic Score: {left_scenic_score}")
+            # print(f"Right Scenic Score: {right_scenic_score}")
+            # print(f"Up Scenic Score: {up_scenic_score}")
+            # print(f"Down Scenic Score: {down_scenic_score}")
+            tree_scenic_score = (
+                left_scenic_score
+                * right_scenic_score
+                * up_scenic_score
+                * down_scenic_score
+            )
+            # print(f"Tree Scenic Score: {tree_scenic_score}")
+            if tree_scenic_score > highest_scenic_score:
+                highest_scenic_score = tree_scenic_score
+                # print(f"Highest Scenic Score (for loop):
+                # {highest_scenic_score}")
+    print(f"Highest Scenic Score: {highest_scenic_score}")
+    return highest_scenic_score
 
 
 def main():
@@ -107,3 +172,7 @@ if __name__ == "__main__":
 # if no, tree is visible - increment visible_direction_count by 1
 # if visible_direction_count = 4, tree is visible
 # increment visible_trees by 1
+
+# part two notes
+# stop at edge or first same or taller tree
+# count number of trees and multiply together
